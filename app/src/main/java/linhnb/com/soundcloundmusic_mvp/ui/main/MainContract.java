10ef192c3b0df905480ae6ab515d558150825a05
Linh <nguyenbalinh06031997@gmail.com>
2018-08-03 20:15:16 +0700

    create splash screen

---
+++ app/src/main/java/linhnb/com/soundcloundmusic_mvp/ui/main/MainContract.java
@@ -0,0 +1,15 @@
+package linhnb.com.soundcloundmusic_mvp.ui.main;
+
+import linhnb.com.soundcloundmusic_mvp.ui.base.BasePresenter;
+import linhnb.com.soundcloundmusic_mvp.ui.base.BaseView;
+
+public interface MainContract {
+
+    interface View extends BaseView<Presenter>{
+
+    }
+    interface Presenter extends BasePresenter{
+
+
+    }
+}
