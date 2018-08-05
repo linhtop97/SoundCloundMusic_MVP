97556855d23c9031f3cf087a17b397bd2dfe35ec
linh <nguyenbalinh06031997@gmail.com>
2018-08-05 16:14:41 +0700

    create task obj and repository base

--- app/src/main/java/linhnb/com/soundcloundmusic_mvp/utils/Constant.java
+++ app/src/main/java/linhnb/com/soundcloundmusic_mvp/utils/Constant.java
@@ -1,6 +1,24 @@
 package linhnb.com.soundcloundmusic_mvp.utils;
 
 public class Constant {
+    // Network
+    public static final String BASE_URL = "https://api-v2.soundcloud.com/";
+    public static final String PARA_MUSIC_GENRE = "charts?kind=top&genre=soundcloud%3Agenres%3A";
+    public static final String PARA_SEARCH_TRACK = "search/tracks?facet=genre&limit=10&linked_partitioning=1&q=";
+    public static final String PARA_OFFSET = "offset";
+    public static final String PARA_STREAM = "stream";
+    public static final String CLIENT_ID = "client_id";
+    public static final String REQUEST_METHOD_GET = "GET";
+    public static final String LIMIT = "limit";
+    public static final int READ_TIME_OUT = 5000; /* milliseconds */
+    public static final int CONNECT_TIME_OUT = 5000; /* milliseconds */
+    public static final String NULL_RESULT = "null";
+    public static final String[] MUSIC_GENRES = {"pop", "all-music", "all-audio",
+            "alternativerock", "ambient", "classical", "country"};
+
+    // String
+    public static final String BREAK_LINE = "\n";
+    public static final String INTERNET_NOT_AVAIABLE = "internet not avaiable";
 
     public static final String ARGUMENT_TIME_DELAY = "ARGUMENT_PLAY_ANIM";
 }
---
+++ app/src/main/java/linhnb/com/soundcloundmusic_mvp/utils/StringUtil.java
@@ -0,0 +1,56 @@
+package linhnb.com.soundcloundmusic_mvp.utils;
+
+import linhnb.com.soundcloundmusic_mvp.BuildConfig;
+
+public class StringUtil {
+    public static String convertUrlFetchMusicGenre(String genre, int limit, int offset) {
+        return String.format("%s%s%s&%s=%s&%s=%d&%s=%d", Constant.BASE_URL,
+                Constant.PARA_MUSIC_GENRE, genre, Constant.CLIENT_ID,
+                BuildConfig.API_KEY, Constant.LIMIT, limit, Constant.PARA_OFFSET, offset);
+    }
+
+    public static String getUrlStreamTrack(String uriTrack) {
+        return String.format("%s/%s?%s=%s", uriTrack, Constant.PARA_STREAM,
+                Constant.CLIENT_ID, BuildConfig.API_KEY);
+    }
+
+    public static String getUrlDownload(String url) {
+        StringBuffer stringBuffer = new StringBuffer(url);
+        stringBuffer.append("/stream?")
+                .append(Constant.CLIENT_ID)
+                .append('=')
+                .append(BuildConfig.API_KEY);
+        return stringBuffer.toString();
+    }
+
+    public static String formatDuration(int duration) {
+        duration /= 1000; // milliseconds into seconds
+        int minute = duration / 60;
+        int hour = minute / 60;
+        minute %= 60;
+        int second = duration % 60;
+        if (hour != 0)
+            return String.format("%2d:%02d:%02d", hour, minute, second);
+        else
+            return String.format("%02d:%02d", minute, second);
+    }
+
+    public static String formatCount(int count) {
+        int number = count / 1000;
+        if (number == 0) {
+            return String.valueOf(count);
+        }
+        return String.format("%dK", number);
+    }
+
+    public static String convertUrlDownloadTrack(String url) {
+        return String.format("%s?%s=%s", url, Constant.CLIENT_ID, BuildConfig.API_KEY);
+    }
+
+    //
+    public static String convertUrlSearchTrack(String trackName) {
+        return String.format("%s%s%s&%s=%d&%s=%s", Constant.BASE_URL, Constant.PARA_SEARCH_TRACK, trackName,
+                Constant.PARA_OFFSET, 100, Constant.CLIENT_ID,
+                BuildConfig.API_KEY);
+    }
+}
